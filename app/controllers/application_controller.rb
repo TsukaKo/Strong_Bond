b30991faff1fa5e3f1e51f204d0a5fecdd8ee369
TsukaKo <Kodaibb3@gmail.com>
2022-03-21 11:21:00 +0000

    [Edit]application.html.erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!,except: [:top, :about]
+
+
+# ログイン後はグループのindexへ
+  def after_sign_in_path_for(resource)
+    groups_path
+  end
+
+# ログアウト後はトップ画面へ
+  def after_sign_out_path_for(resource)
+    root_path
+  end
 
   protected
 
